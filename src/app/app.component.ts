2ca8af293bd539a4aef4748c08cee32457dad428
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-11-30 21:43:39 +0200

    component 2 binging one way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   number = 42;
   arr = [1, 2, 3];
   obj = {a: 1, b: {c: 2}};
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
+    }, 3000);
+  }
 }
