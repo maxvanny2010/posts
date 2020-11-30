3ef29c8956e5ef19e64699bde2740f458676b899
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-11-30 21:28:58 +0200

    component 2 интерполяция

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule
