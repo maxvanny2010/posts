56fb663e6b414a91d9748c14f27faf685222d199
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-11-30 22:20:19 +0200

    component 2 binging two way(click, press up/enter)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,44 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
-  obj = {a: 1, b: {c: 2}};
-  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png';
+  // title = 'Dynamic title';
+  // number = 42;
+  // arr = [1, 2, 3];
+  // obj = {a: 1, b: {c: 2}};
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png';
+  value = '';
+  key = '';
+  enter = '';
 
   constructor() {
-    setTimeout(() => {
+    /* setTimeout(() => {
       console.log('Timeout is over');
-      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
-    }, 3000);
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg
+      /240px-Angular_full_color_logo.svg.png';
+    }, 3000); */
+  }
+
+  onInput($event: any): any {
+    console.log('Event', $event);
+    this.value = $event.target.value;
+  }
+
+  onClick(): any {
+    console.log('Click');
+  }
+
+  onKeyBoardUp($event: KeyboardEvent): any {
+    console.log('Up', $event);
+    this.key = ($event.target as HTMLInputElement).value;
+  }
+
+  onKeyBoardEnter($event: any): any {
+    console.log('Enter', $event);
+    this.enter = ($event.target as HTMLInputElement).value;
+  }
+
+  onBlur(str: string): any {
+    this.value = str;
+
   }
 }
