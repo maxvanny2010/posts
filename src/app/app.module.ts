898aa2d90bb4bb8364864f4bf470dfcf1fe1d156
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-11-30 23:37:23 +0200

    directive: ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-
 import {CommonModule} from '@angular/common';
+
+import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {PostComponent} from './post/post.component';
