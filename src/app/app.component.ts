70f1617885204d9a2b78d3c4473a6ad554696e85
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-01 22:07:53 +0200

    block2 components *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Первый компонент', text: 'Текст первого компонента', id: 1},
+    {title: 'Второй компонент', text: 'Текст второго компонента', id: 2},
+    {title: 'Третий компонент', text: 'Текст третьего компонента', id: 3}
+  ];
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {FormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
