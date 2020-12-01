5d894d086b976dc07cfe3d7d0407d582bae57ff9
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-01 21:38:03 +0200

    block2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {PostComponent} from './post/post.component';
-import {Post2Component} from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post2Component
   ],
   imports: [
     BrowserModule,
