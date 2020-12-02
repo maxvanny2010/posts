7e21edb1afe82d5dca9a696c935570a1f9fb19b7
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-02 22:02:59 +0200

    block4 pipes: <pre>{{obj | json}}<pre>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ export class AppComponent {
   e: number = Math.E;
   str = 'Hello world';
   date: Date = new Date();
+  float = 0.42;
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        d: 4
+      }
+    }
+  };
 }
