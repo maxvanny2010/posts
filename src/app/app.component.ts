08daacd1e456bd32ce2a8716d61221347928252a
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-07 13:56:57 +0200

    block 8 server : get<Todo[]> post<Todo>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+  todoTitle = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        console.log('RESPONSE: ', todos);
+        this.todos = todos;
+      });
+  }
+
+  addTodo(): void {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo = {
+      title: this.todoTitle,
+      completed: false
+    };
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log(todo);
+        this.todos.push(todo);
+        this.todoTitle = '';
+      });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import {AppComponent} from './app.component';
     BrowserModule,
     CommonModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
