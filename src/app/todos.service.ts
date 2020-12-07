026ff8071273df22b7bcf76749fec148163ba4cc
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-07 15:42:40 +0200

    block 8 server :service catchError and throwError in pipe(it is false path)

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Todo} from './app.component';
-import {Observable} from 'rxjs';
-import {delay} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
+import {catchError, delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,12 @@ export class TodosService {
   }
 
   fetch(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos9?_limit=2').pipe(delay(500));
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos9?_limit=2')
+      .pipe(
+        delay(500),
+        catchError(error => {
+          return throwError(error);
+        }));
   }
 
   remove(id: number): Observable<void> {
