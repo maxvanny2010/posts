68fbee4c494fa08fe5eeb962dba5db32212cc6a9
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-08 20:12:33 +0200

    block 9 routing : GuardChild

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,10 +1,10 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private auth: AuthService, private router: Router) {
   }
 
@@ -13,5 +13,10 @@ export class AuthGuard implements CanActivate {
     return this.auth.isAuthenticated().then(isAuth => {
       return isAuth ? true : this.router.navigate(['/'], {queryParams: {auth: false}});
     });
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    : Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(childRoute, state);
   }
 }
