5d437b7b9aaa6793f4084217a9288ff822223d66
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-08 19:12:33 +0200

    block 9 routing : button queryParam !!query.showId

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,12 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostsService} from '../posts.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {
+export class PostsComponent implements OnInit {
+  showId = false;
+
+  constructor(public postsService: PostsService, private route: ActivatedRoute) {
   }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((query) => {
+      console.log(query);
+      this.showId = !!query.showId;
+    });
+  }
+
 }
