29ccbaf9ff631fb3587be7362905f42f9a406a71
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-09 19:23:02 +0200

    clean

--- src/app/about-extra/about-extra.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-about-extra',
-  templateUrl: './about-extra.component.html',
-  styleUrls: ['./about-extra.component.css']
-})
-export class AboutExtraComponent {
-}
